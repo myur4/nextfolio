4489e5ca446cf6e39e852549adc357c7bdda970f
George Santos <47012804+myur4@users.noreply.github.com>
2022-12-15 12:36:34 -0300

    Quote padronization

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,29 +1,29 @@
-import myPic from '../public/img/avatar.png'
-import Image from 'next/image'
-import { GithubIcon, DiscordIcon, SpotifyIcon, SoundcloudIcon } from '@remixicons/react/fill'
-import { motion } from 'framer-motion'
-import { Typewriter } from 'react-simple-typewriter'
+import myPic from '../public/img/avatar.png';
+import Image from 'next/image';
+import { GithubIcon, DiscordIcon, SpotifyIcon, SoundcloudIcon } from '@remixicons/react/fill';
+import { motion } from 'framer-motion';
+import { Typewriter } from 'react-simple-typewriter';
 import { fade_out, zoom_in } from '../utils/variants';
 
 const socials = [
-    { component: <GithubIcon className='w-16 fill-white hover:fill-secondary cursor-pointer' />, url: 'https://github.com/myur4' },
-    { component: <DiscordIcon className='w-16 fill-blue-500 hover:fill-secondary cursor-pointer' />, url: '' },
-    { component: <SoundcloudIcon className='w-14 fill-red-400 hover:fill-secondary hover:border-secondary cursor-pointer border-2 border-red-400 rounded-full' />, url: 'https://soundcloud.com/von-zodd' },
-    { component: <SpotifyIcon className='w-16 fill-green-400 hover:fill-secondary cursor-pointer' />, url: '' },
+    { component: <GithubIcon className="w-16 fill-white hover:fill-secondary cursor-pointer" />, url: "https://github.com/myur4" },
+    { component: <DiscordIcon className="w-16 fill-blue-500 hover:fill-secondary cursor-pointer" />, url: "" },
+    { component: <SoundcloudIcon className="w-14 fill-red-400 hover:fill-secondary hover:border-secondary cursor-pointer border-2 border-red-400 rounded-full" />, url: "https://soundcloud.com/von-zodd" },
+    { component: <SpotifyIcon className="w-16 fill-green-400 hover:fill-secondary cursor-pointer" />, url: "" },
 ]
 
 export default function Banner() {
     return (
-        <section id='home' aria-label='introduction banner with socials'>
+        <section id="home" aria-label="introduction banner with socials">
             <div className="min-h-screen flex flex-col md:flex-row p-8 items-center justify-center gap-6 relative">
 
-                <div className='flex flex-col justify-center items-center'>
-                    <span className='text-6xl font-extrabold text-primary'>
+                <div className="flex flex-col justify-center items-center">
+                    <span className="text-6xl font-extrabold text-primary">
                         <Typewriter
                             loop={true}
-                            words={['Fullstack', 'React', '.NET', 'Passionate']}
+                            words={["Fullstack", "React", ".NET", "Passionate"]}
                             cursor
-                            cursorStyle='_'
+                            cursorStyle="_"
                             delaySpeed={1100}
                         />
                     </span>
@@ -31,16 +31,16 @@ export default function Banner() {
                         initial={{ opacity: 0, scale: 0.5 }}
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{ duration: 0.75 }}>
-                        <Image src={myPic} className='' alt="" />
+                        <Image src={myPic} className="" alt="" />
                     </motion.div>
-                    <h1 className='text-6xl md:text-7xl text-primary font-extrabold'>Developer.</h1>
+                    <h1 className="text-6xl md:text-7xl text-primary font-extrabold">Developer.</h1>
                 </div>
 
                 <motion.ul
-                    className='flex md:flex-col gap-4 items-center'
+                    className="flex md:flex-col gap-4 items-center"
                     variants={zoom_in}
-                    initial='hidden'
-                    animate='visible'
+                    initial="hidden"
+                    animate="visible"
                 >
 
                     {
--- components/basenav.tsx
+++ components/basenav.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 export default function BaseNav() {
@@ -9,24 +9,24 @@ export default function BaseNav() {
         <nav className="fixed py-2 px-5 z-50 w-4/5 mx-auto bg-slate-800 top-2 left-0 right-0 text-center rounded-full">
 
             <div className="flex justify-between items-center">
-                <a href='#home' className="text-2xl md:text-3xl font-extrabold text-primary hover:underline hover:text-accent cursor-pointer">George Santos</a>
-                <div className='flex gap-2 items-center'>
-                    <AiOutlineMenu className={`${open ? 'hidden' : ''} lg:hidden w-auto h-6 fill-primary cursor-pointer`} onClick={() => setOpen(!open)} />
-                    <ul className={`${open ? 'flex flex-col' : 'hidden'} lg:flex lg:flex-row gap-2`}>
-                        <li className='lg:px-4 lg:py-3 text-sm bg-primary text-black font-bold rounded-3xl hover:bg-black hover:text-primary cursor-pointer'>
-                            <a href="#intro" className='block'>Introduction</a>
+                <a href="#home" className="text-2xl md:text-3xl font-extrabold text-primary hover:underline hover:text-accent cursor-pointer">George Santos</a>
+                <div className="flex gap-2 items-center">
+                    <AiOutlineMenu className={`${open ? "hidden" : ""} lg:hidden w-auto h-6 fill-primary cursor-pointer`} onClick={() => setOpen(!open)} />
+                    <ul className={`${open ? "flex flex-col" : "hidden"} lg:flex lg:flex-row gap-2`}>
+                        <li className="lg:px-4 lg:py-3 text-sm bg-primary text-black font-bold rounded-3xl hover:bg-black hover:text-primary cursor-pointer">
+                            <a href="#intro" className="block">Introduction</a>
                         </li>
-                        <li className='px-4 lg:px-2 lg:py-3 text-sm bg-primary text-black font-bold rounded-3xl hover:bg-black hover:text-primary cursor-pointer'>
+                        <li className="px-4 lg:px-2 lg:py-3 text-sm bg-primary text-black font-bold rounded-3xl hover:bg-black hover:text-primary cursor-pointer">
                             <a href="#about">About</a>
                         </li>
-                        <li className='lg:px-4 lg:py-3 text-sm bg-primary text-black font-bold rounded-3xl hover:bg-black hover:text-primary cursor-pointer'>
+                        <li className="lg:px-4 lg:py-3 text-sm bg-primary text-black font-bold rounded-3xl hover:bg-black hover:text-primary cursor-pointer">
                             <a href="#skill">Skills</a>
                         </li>
-                        <li className='px-4 lg:px-2 lg:py-3 text-sm bg-primary text-black font-bold rounded-3xl hover:bg-black hover:text-primary cursor-pointer'>
+                        <li className="px-4 lg:px-2 lg:py-3 text-sm bg-primary text-black font-bold rounded-3xl hover:bg-black hover:text-primary cursor-pointer">
                             <a href="#projects">Projects</a>
                         </li>
                     </ul>
-                    <AiOutlineClose className={`${open ? '' : 'hidden'} lg:hidden w-auto h-6 fill-primary cursor-pointer`} onClick={() => setOpen(!open)} />
+                    <AiOutlineClose className={`${open ? "" : "hidden"} lg:hidden w-auto h-6 fill-primary cursor-pointer`} onClick={() => setOpen(!open)} />
                 </div>
 
             </div>
