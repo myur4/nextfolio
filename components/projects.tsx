c54ef1c44956659102eaddfde5b683ddadcb86eb
George Santos <47012804+myur4@users.noreply.github.com>
2022-12-17 01:58:39 -0300

    Create project section

---
+++ components/projects.tsx
@@ -0,0 +1,11 @@
+import { SiDotnet, SiReact, SiVuedotjs, SiPostgresql, SiRedis, SiMongodb, SiTypescript, SiCsharp, SiSqlite, SiRubygems } from 'react-icons/si';
+import { useInView } from 'framer-motion';
+import { useRef } from 'react';
+
+export default function Projects() {
+    return (
+        <section id="projects" className="flex items-center flex-col flex-wrap gap-8 justify-center max-w-5xl mx-auto min-h-screen">
+            <div>
+            </div>
+        </section>)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,16 @@ import BaseBanner from '../components/banner';
 import Introduction from '../components/intro'
 import Skills from '../components/skillContainer'
 import Personal from '../components/about'
+import Projects from '../components/projects';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <BaseBanner/>
       <Introduction/>
       <Personal/>
       <Skills/>
-    </div>
+      <Projects/>
+    </>
   )
 }
