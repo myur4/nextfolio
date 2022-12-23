70f5f0d1bf6d15e47a4e53e58aacdfd7e462921d
George Santos <47012804+myur4@users.noreply.github.com>
2022-12-23 12:41:04 -0300

    Update skillContainer.tsx

--- components/skillContainer.tsx
+++ components/skillContainer.tsx
@@ -1,20 +1,14 @@
-import { SiDotnet, SiReact, SiVuedotjs, SiPostgresql, SiRedis, SiMongodb, SiTypescript, SiCsharp, SiSqlite, SiRubygems } from 'react-icons/si';
-import { useInView } from 'framer-motion';
-import { useRef } from 'react';
+import { SiDotnet, SiReact, SiVuedotjs, SiTypescript, SiCsharp, SiRubygems, SiGoland } from 'react-icons/si';
 
 export default function Skills() {
-
-    const ref = useRef(null);
-    const isInView = useInView(ref);
-
     return (
-        <section id="skill" ref={ref} className="flex items-center flex-col flex-wrap gap-8 justify-center max-w-5xl mx-auto min-h-screen">
+        <section id="skill" className="flex items-center flex-col flex-wrap gap-8 justify-center max-w-5xl mx-auto min-h-screen">
 
             <h1 className="text-3xl lg:text-5xl text-primary font-extrabold underline">Skills</h1>
-            <div className='flex gap-2'>
-            <span className='badge-secondary badge-md lg:badge-lg rounded-full'>Fast Learner</span>
-            <span className='badge-secondary badge-md lg:badge-lg rounded-full'>Good Companion</span>
-            <span className='badge-secondary badge-md lg:badge-lg rounded-full'>Problem Solver</span>
+            <div className='flex gap-2 flex-wrap'>
+            <span className='badge-secondary badge-sm lg:badge-lg rounded-full'>Fast Learner</span>
+            <span className='badge-secondary badge-sm lg:badge-lg rounded-full'>Good Companion</span>
+            <span className='badge-secondary badge-sm lg:badge-lg rounded-full'>Problem Solver</span>
             </div>
 
             <div className='flex flex-col gap-4 flex-wrap'>
@@ -25,8 +19,8 @@ export default function Skills() {
                         <h1 className='text-xl font-bold'>C#</h1>
                     </div>
                     <div className='flex flex-col gap-2 items-center'>
-                        <SiRubygems className='w-auto h-16 lg:h-36 border-2 rounded-full p-4 fill-red-500' />                        
-                        <h1 className='text-xl font-bold'>Ruby</h1>
+                        <SiGoland className='w-auto h-16 lg:h-36 border-2 rounded-full p-4 fill-blue-300'/>
+                        <h1 className='text-xl font-bold'>Golang</h1>
                     </div>
                     <div className='flex flex-col gap-2 items-center'>
                         <SiTypescript className='w-auto h-16 lg:h-36 border-2 rounded-full p-4 fill-blue-400' />
